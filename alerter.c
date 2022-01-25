84362b55329f45f84a2550b997cd4f8fd9254fa3
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-01-25 23:28:29 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -4,22 +4,25 @@
 int alertFailureCount = 0;
 #define Alert_Threshold 200
 
+float farenheitToCelcius(float farenheit);
+
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    if(celcius > Alert_Threshold){
+    if(celcius > Alert_Threshold) {
         printf("ALERT: Temperature is %.1f celcius.\n", celcius);
         return 500;
-    }else{
+    } else {
         return 200;
-    
+    }
 }
 
-float farenheitToCelcius(float farenheit){
+float farenheitToCelcius(float farenheit) {
     return ((farenheit-32)*5/9);
 }
+    
 void alertInCelcius(float farenheit) {
     float celcius = farenheitToCelcius(farenheit);
     int returnCode = networkAlertStub(celcius);

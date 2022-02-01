d96831f003af19c042a4154a3f40ebd3b19f0966
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-01 21:32:24 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+#include "alerter.h"
 
 int alertFailureCount = 0;
-#define Alert_Threshold 200
-
-float ConvertFarenheitToCelcius(float farenheit);
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
@@ -18,7 +16,7 @@ int networkAlertStub(float celcius) {
     }
 }
 
-float ConvertFarenheitToCelcius(float farenheit) {
+float FarenheitToCelciusConverter(float farenheit) {
     return ((farenheit-32)*5/9);
 }
     
@@ -30,19 +28,23 @@ void alertInCelcius(float farenheit, int (*Fn_Ptr_NetworkAlert)(float)) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
+}
+
+void testTempAlerter(int expectedFailCount) {
+    assert(alertFailureCount == expectedFailCount);
 }
 
 int main() {
     int (*Fcn_Ptr_networkAlert)(float);
     Fcn_Ptr_networkAlert = &networkAlertStub;
     alertInCelcius(303.6,Fcn_Ptr_networkAlert);
-    assert(alertFailureCount==0);
+    testTempAlerter(0);
     alertInCelcius(400.5,Fcn_Ptr_networkAlert);
-    assert(alertFailureCount==1);
+    testTempAlerter(1);
     alertInCelcius(500.4,Fcn_Ptr_networkAlert);
-    assert(alertFailureCount==2);
+    testTempAlerter(2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;

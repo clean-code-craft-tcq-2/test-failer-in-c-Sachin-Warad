c6011c600ffe986150b63df865c5f445ae1f29a2
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-01 23:12:05 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,41 +1,26 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
+#include "misaligned.h"
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-struct ColorCode
-{
-    int pairNumber;
-    char MajorColor[15];
-    char MinorColor[15];
-};
-
-ColorCode Manual[25];
-
-void printOnConsole(int MajorColorNumber, int MinorColorNumber);
-
-int printColorMap() {
-    int i = 0, j = 0,index=0;
+int printColorMap(void (*Fn_Ptr_ReferenceManual)(int,int)) {
+    int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            Manual[index].pairNumber = i*5+j;
-            strcpy(Manual[index].MajorColor, majorColor[i]);
-            strcpy(Manual[index].MinorColor, minorColor[j]);
-            printOnConsole(i,j);
-            index++;
+            Fn_Ptr_ReferenceManual(i,j);
         }
     }
     return i * j;
 }
 void printOnConsole(int MajorColorNumber, int MinorColorNumber)
 {
-    printf("%d | %s | %s\n", MajorColorNumber * 5 + MinorColorNumber, majorColor[MajorColorNumber], minorColor[MajorColorNumber]);
+    printf("%d | %s | %s\n", MajorColorNumber * 5 + MinorColorNumber + 1, majorColor[MajorColorNumber], minorColor[MajorColorNumber]);
 }
 
 int main() {
-    int result = printColorMap();
+    void (*Fn_Ptr_ReferenceManual)(int,int);
+    Fn_Ptr_ReferenceManual = &testColorManual;
+    int result = printColorMap(Fn_Ptr_ReferenceManual);
     assert(result == 25);
     assert(Manual[2].pairNumber == 2);
     assert(strcmp(Manual[2].MajorColor,"White") == 0);

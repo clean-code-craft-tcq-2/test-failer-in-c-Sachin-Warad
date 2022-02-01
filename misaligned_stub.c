d9a8549852dbcfcbfe6d3f3a99758b064dabfeb7
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-01 22:44:48 +0530

    Create misaligned_stub.c

---
+++ misaligned_stub.c
@@ -0,0 +1,10 @@
+#include "misaligned_stub.h"
+
+ColorCode Manual[25];
+
+void testColorManual(int MajorColorNumber, int MinorColorNumber) {
+      Manual[index].pairNumber = (i*5)+j+1;
+      strcpy(Manual[index].MajorColor, majorColor[i]);
+      strcpy(Manual[index].MinorColor, minorColor[j]);
+      index++;
+}

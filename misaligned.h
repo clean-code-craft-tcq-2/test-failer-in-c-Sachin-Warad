494f8373a817f366fb476d0624383952fae5fc2d
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-01 22:46:22 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+struct ColorCode
+{
+    int pairNumber;
+    char MajorColor[15];
+    char MinorColor[15];
+};
+
+void testColorManual(int MajorColorNumber, int MinorColorNumber);
 void printOnConsole(int MajorColorNumber, int MinorColorNumber);

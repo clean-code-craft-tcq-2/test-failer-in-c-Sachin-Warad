8d633deb01110ee309dea69ee08dd71ab4d04e64
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-03 23:07:18 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,6 +1,6 @@
 float farenheitToCelciusConverter(float farenheit);
 int networkAlertStub(float celcius);
 void alertInCelcius(float farenheit, int (*Fn_Ptr_NetworkAlert)(float));
-void testTempAlerter(int expectedFailCount);
+void testTempAlerter(float farenheit, int expectedFailCount);
 
 #define Alert_Threshold 200
